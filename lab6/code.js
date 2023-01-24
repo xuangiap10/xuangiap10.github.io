62938cf774f26d16eac78858d58f52aedb2e0d61
xuangiap10 <xuangiap10@gmail.com>
2023-01-23 21:07:45 -0600

    lab6

---
+++ lab6/code.js
@@ -0,0 +1,23 @@
+    const lab6part1 = {
+        /* 4 Define a function sum() and a function multiply() that sums and multiplies 
+        (respectively) all the numbers in an array of numbers*/
+        sum: function (x){
+            return x.reduce((x,y)=>x+y, 0);
+        },
+        multiply: function (x){
+            return x.reduce((x,y)=>x*y, 1);
+        },
+        /* 5 Define a function reverse() that computes the reversal of a string.*/
+        reverse: function (x){
+            let ret="";
+            for(let i = x.length-1; i>=0; i--){
+                ret += x.charAt(i);
+            }
+            return ret;
+        },
+        /* 7 Write a function filterLongWords() that takes an array of words and an integer i 
+        and returns the array of words that are longer than i. */
+        filterlongWords: function (x, i){
+            return x.filter(s=>s.length>i);
+        }
+    }
---
+++ lab6/tests.js
@@ -0,0 +1,9 @@
+
+/*1 sum */
+arr=[1,3,5,6,8];
+console.log("sum(" + arr + "): " + lab6part1.sum(arr));
+console.log("multiply(" + arr + "): " + lab6part1.multiply(arr));
+s = "maharishi";
+console.log("reverse(" + s + "): " + lab6part1.reverse(s));
+list = ["jan", "feb", "march", "april", "may"];
+console.log("filterlongWords([" + list + "], 3): " + lab6part1.filterlongWords(list,3));
