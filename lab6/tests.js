c9781119d54b91c69f9170b9b685f87aa7209c86
xuangiap10 <xuangiap10@gmail.com>
2023-01-23 23:24:13 -0600

    fix

--- lab6/tests.js
+++ lab6/tests.js
@@ -1,19 +1,9 @@
 
-/*1 sum */
-arr=[1,3,5,6,8];
-console.log("sum(" + arr + "): " + lab6part1.sum(arr));
-console.log("multiply(" + arr + "): " + lab6part1.multiply(arr));
-s = "maharishi";
-console.log("reverse(" + s + "): " + lab6part1.reverse(s));
-list = ["jan", "feb", "march", "april", "may"];
-console.log("filterlongWords([" + list + "], 3): " + lab6part1.filterlongWords(list,3));
-
-
-
 function verify(){
     let arr=[1,3,5,6,8];
     let s = "maharishi";
-    let list = ["jan", "feburary", "march", "april", "may"];;
+    let list = ["jan", "feburary", "march", "april", "may"];
+
     describe("sum", function(){
         it("sum all the numbers in an array of numbers", function(){
             assert.equal(lab6part1.sum(arr), 23);
@@ -35,6 +25,5 @@ function verify(){
             assert.deepEqual(lab6part1.filterlongWords(list,3),[ 'feburary', 'march', 'april' ]);
         });
     });
-    mocha.run();
 }
-window.onload = verify;
+//window.onload = verify;
