5347190b301fe3fa1b31d846e1dcbe722d8ef9eb
xuangiap10 <xuangiap10@gmail.com>
2023-02-01 01:59:40 -0600

    Update maze.js

--- lab11/Maze/maze.js
+++ lab11/Maze/maze.js
@@ -1,42 +1,30 @@
 
 $(()=>{
-    let maze = $("#maze");
     let start = $("#maze #start");
     let gameon = 0;
-   var startX = 0; var startY = 0;
-   maze.mousemove((evt)=>{
-        
-       //$("#info").text(evt.target.id + "left: " + evt.offsetX + " top: " + evt.offsetY +  " pX: " + evt.pageX + " pY: " + evt.pageY +" CX: " + evt.clientX + " CY: " + evt.clientY) ;
-        //evt.stopPropagation();
-        if ( gameon == 1){
-            //let left = evt.offsetX - startX;
-            //let top = evt.offsetY - startY;
-            let left = evt.offsetX - startX;
-            let top = evt.offsetY - startY;
-            //$("#info").text("left: " + left + " top: " + top +  " pX: " + evt.pageX + " pY: " + evt.pageY +" CX: " + evt.clientX + " CY: " + evt.clientY) ;
-            if (left > 0 && top > 0){
-                start.css({
-                    "left": left,
-                    "top":  top
-                });
-            }
-        }
-   }
-   );
    start.on ({
-    click: (evt)=>{
-        if(gameon == 1) return;
-        gameon = 1;
-        $("#status").text("...Playing...");
-        startX = evt.offsetX; startY = evt.offsetY;
-    },
+        "click": (evt)=>{
+            if(gameon == 1) return;
+            gameon = 1;
+            $("#status").text("...Playing...");
+            $("#maze .boundary").css({"background-color": "#eeeeee"})
+        },
+        "mousemove": (evt)=> {
+            if ( gameon == 1){
+                let left = parseInt(start.css("left")) + evt.offsetX - 15 ;
+                let top = parseInt(start.css("top"))  + evt.offsetY - 15 ;
+                start.css({"left": left, "top":  top});
+            }
+        },
     });
+
     $("#maze .boundary").on({
         mouseenter: ()=>{
             if ( gameon == 1){
                 gameon = 0;
                 start.css({"left": 0, "top" : 205});
-                $("#status").text("You Lose");
+                $("#status").text("You Lose - Click the \"S\" to begin.  ");
+                $("#maze .boundary").css({"background-color": "orange"})
             }
         },
     });
@@ -45,7 +33,8 @@ $(()=>{
             if ( gameon == 1){
                 gameon = 0;
                 start.css({"left": 0, "top" : 205 });
-                $("#status").text("You Win");
+                $("#status").text("You Win - Click the \"S\" to begin.");
+                $("#maze .boundary").css({"background-color": "green"})
             }
         }
     });
